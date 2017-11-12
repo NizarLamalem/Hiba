215591284c1e79b2df12a39bbe7024f7e01ee22b
NizarLamalem <lamanizar12@gmail.com>
2017-11-12 13:45:57 +0000

    Hiba Project

---
+++ Hiba/src/application/MainPageController.java
@@ -0,0 +1,25 @@
+package application;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class MainPageController implements Initializable{
+
+    @FXML
+    private JFXHamburger hamburger;
+
+    @FXML
+    private JFXDrawer drawer;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
