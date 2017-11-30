a1196ff265900014de3a58d9f741966e2effed0f
NizarLamalem <lamanizar12@gmail.com>
2017-11-30 02:51:48 +0000

    hiba

--- Hiba/src/application/DrawerController.java
+++ Hiba/src/application/DrawerController.java
@@ -1,16 +1,21 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class DrawerController {
+public class DrawerController implements Initializable {
 
 	@FXML
 	private JFXButton nouvelleFacture;
@@ -25,16 +30,19 @@ public class DrawerController {
 	private JFXButton Exite;
 
 	@FXML
+	private ImageView back;
+
+	@FXML
 	void onClick(MouseEvent event) throws IOException {
 
 		String Destination = "Null";
 		Object Tmp = event.getSource();
-		
+
 		MainPageController.transition.setRate(-1);
 		MainPageController.transition.setRate(-1);
 		MainPageController.transition.play();
 		MainPageController.transition.play();
-		
+
 		if (Tmp == nouvelleFacture) {
 			// todo Tratment Nouvelle Facture ;
 			Destination = "AddFacture";
@@ -55,7 +63,6 @@ public class DrawerController {
 				}
 			}
 		}
-		
 
 		MainPageController.Drawer.close();
 		MainPageController.RootP.toFront();
@@ -63,10 +70,15 @@ public class DrawerController {
 			MainPageController.RootP.getChildren().setAll(
 					(AnchorPane) FXMLLoader.load(getClass().getResource("/interfaces/" + Destination + ".fxml")));
 			Destination = "null";
-			
+
 		}
 
+	}
 
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		back.setImage(new Image("/Resources/back.jpg"));
 	}
 
 }
